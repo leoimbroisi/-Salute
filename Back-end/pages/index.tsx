91dd0ba2b4711be427c5f7ca486a6282f2ec4160
Leonardo Imbroisi Mesquita <leonardo.imbroisi@gmail.com>
2025-12-22 14:48:40 -0300

    Update backend page title to +Salute

--- Back-end/pages/index.tsx
+++ Back-end/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>Sistema de Gestão de Dados de Saúde - API</h1>
+      <h1>+Salute - Sistema de Gestão de Dados de Saúde - API</h1>
       <p>Backend Next.js rodando na porta 3001</p>
       <p style={{ marginTop: '20px', color: '#666' }}>
         Os índices do Elasticsearch serão criados automaticamente na primeira requisição.
